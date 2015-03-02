09ee26172e53ca8297f6783521c34ab51fd72741
Mark Palange <mpalange@lumoslabs.com>
2015-03-02 13:42:51 -0800

    Changed hierarchy

--- util/make_zxp.sh
+++ util/make_zxp.sh
@@ -1,8 +1,10 @@
 #! /bin/bash -ex
+
 #XMAN_CMD="/Applications/Adobe Extension Manager CC/Adobe Extension Manager CC.app/Contents/MacOS/Adobe Extension Manager CC"
 #"$XMAN_CMD" -suppress -package mxi=../ExpelliartusMaximus.mxi zxp=../bin/ExpelliartusMaximus.zxp
 #echo $?
 
 #java -jar ucf.jar -package -storetype PKCS12 -keystore myCert.p12 -storepass mypasswd -tsa https://timestamp.geotrust.com/tsa myExtension.zxp -C ./myExtension .
 #java -jar signingtoolkit/ucf.jar -package -storetype PKCS12 -keystore ../cert.p12 -storepass lumosity -tsa https://timestamp.geotrust.com/tsa ../bin/ExpelliartusMaximus.zxp -C .. .
-./ZXPSignCmd -sign .. ../bin/ExpelliartusMaximus.zxp ../cert.p12 lumosity -tsa https://timestamp.geotrust.com/tsa
+rm ../bin/ExpelliartusMaximus.zxp
+./ZXPSignCmd -sign ../ExpelliartusMaximus ../bin/ExpelliartusMaximus.zxp ../ExpelliartusMaximus/cert.p12 lumosity -tsa https://timestamp.geotrust.com/tsa
